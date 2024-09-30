4a108d823d3caa47ba88e4236be6866f6afff54a
Pachara004 <147172411+Pachara004@users.noreply.github.com>
2024-09-30 09:16:49 +0700

    del editprofile.dart

--- ishowspeed/lib/pages/User/profile.dart
+++ ishowspeed/lib/pages/User/profile.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:ishowspeed/pages/User/editprofile.dart';
 import 'package:path/path.dart';
 import 'package:ishowspeed/pages/login.dart';
 
